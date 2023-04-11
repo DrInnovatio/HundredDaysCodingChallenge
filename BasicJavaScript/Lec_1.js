5eaa4cc50634b744e315f8b7b59f3138702356f9
DrInnovatio <turing1723@gmail.com>
2023-04-11 15:10:32 +0900

    made an object

--- BasicJavaScript/Lec_1.js
+++ BasicJavaScript/Lec_1.js
@@ -4,5 +4,9 @@ let age = 32;
 let name = "James";
 let habbies = ['sports', 'reading', 'music'];
 
+// made an object
+
+let job = { title: 'DevOps', place: 'New York', salary: 80000}
+
 alert(habbies)
 alert(habbies[1])
