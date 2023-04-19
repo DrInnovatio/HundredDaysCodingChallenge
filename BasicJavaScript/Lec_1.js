df9f111141e90909484427256a415cf0a511969d
DrInnovatio <turing1723@gmail.com>
2023-04-19 10:45:07 +0900

    added excercise.js

--- BasicJavaScript/Lec_1.js
+++ BasicJavaScript/Lec_1.js
@@ -1,12 +1,34 @@
 // Array
 
-let age = 32;
 let name = "James";
 let habbies = ['sports', 'reading', 'music'];
 
 // made an object
 
-let job = { title: 'DevOps', place: 'New York', salary: 80000}
+let job = {
+  title: 'DevOps',
+  place: 'New York',
+  salary: 80000
+};
 
-alert(habbies)
-alert(habbies[1])
+// alert(habbies);
+// alert(habbies[1]);
+// alert(job.salary);
+
+// alert("The End");
+
+let totalAdultYears;
+let age = 41;
+let userAge = 42;
+
+function calculateAdultYears(userAge) {
+  return userAge - 18;
+}
+
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYears(age);
+
+alert(totalAdultYears);
